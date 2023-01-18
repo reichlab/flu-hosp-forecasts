fa6edc81e59c69abb4f8e15ca1a4dab2c9062aa6
lshandross <57642277+lshandross@users.noreply.github.com>
2023-01-18 16:57:19 -0500

    uncomment fc.yml code for actions testing

--- code/format-upload-truth.R
+++ code/format-upload-truth.R
@@ -16,8 +16,8 @@ the_models <- models(zoltar_connection, project_url)
 
 # Format truth data
 source("code/format_truth.R")
-#raw_truth <- read_csv("../Flusight-forecast-data/data-truth/truth-Incident Hospitalizations.csv")
-#truth_date <- floor_date(today(), unit="weeks", week_start=getOption("lubridate.week.start", 1))
+# raw_truth <- read_csv("../Flusight-forecast-data/data-truth/truth-Incident Hospitalizations.csv")
+truth_date <- floor_date(today(), unit="weeks", week_start=getOption("lubridate.week.start", 1))
 truth_csv_path <- paste("data-truth/truth_inc-hosp_", truth_date, ".csv", sep="")
 #formatted_truth <- format_truth(raw_truth, date_column="date", unit_column="location", value_column = "value", target_name="1 wk ahead inc flu hosp", start_date=as.Date("2021-12-13"), save_file=TRUE, save_file_path=truth_csv_path)
 
