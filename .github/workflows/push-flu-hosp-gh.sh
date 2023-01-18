1c07e32de666c23390154fc21d596990c54d5ca9
lshandross <57642277+lshandross@users.noreply.github.com>
2023-01-18 15:25:37 -0500

    Add push-flu-hosp-gh.sh

---
+++ .github/workflows/push-flu-hosp-gh.sh
@@ -0,0 +1,24 @@
+#!/bin/sh
+
+setup_git() {
+  git config --global user.email "git@github.com"
+  git config --global user.name "Github Actions CI"
+}
+
+commit_website_files() {
+  echo "Commiting files..."
+  git add .
+  git commit --message "Github Actions build: $GITHUB_RUN_NUMBER"
+}
+
+upload_files() {
+  echo "Uploading files..."
+  git fetch
+  git pull --rebase https://${GITHUB_PAT}@github.com/reichlab/flu-hosp-forecasts.git
+  git push https://${GITHUB_PAT}@github.com/reichlab/flu-hosp-forecasts.git HEAD:main
+  echo "pushed to github"
+}
+
+setup_git
+commit_website_files
+upload_files
